2f228cbb4e3a93e7bda744ca10ffc03be1f79467
taklist <bjorn.janson.94@gmail.com>
2016-10-21 11:15:16 +0200

    added small script to keep track of my hours worked

---
+++ add_work_journal.py
@@ -0,0 +1,43 @@
+import time
+import sys
+import os.path
+
+try:
+    hours = int(input('Enter hours of work: '))
+    if hours > 10:
+        if raw_input('Really? y/n: ') != 'y':
+            sys.exit()
+            
+except NameError:
+    print 'Invalid input'
+    sys.exit()
+except SyntaxError:
+    print 'Invalid input'
+    sys.exit()
+
+
+contents = []
+while True:
+    try:
+        line = raw_input("")
+    except EOFError:
+        break
+    contents.append(line)
+
+desc = ''
+for line in contents:
+    desc += line
+    desc += '\n'
+
+    
+i=1
+while True:
+    if os.path.exists(time.strftime('../work_journal/%d%m%y_') + str(i)):
+        i+=1
+    else:
+        break
+    
+
+f = open(time.strftime('../work_journal/%d%m%y_' + str(i)), 'w+')
+f.write(str(hours) + '\n\n' + desc + '\n')
+f.close()
---
+++ worktime.py
@@ -0,0 +1,19 @@
+import os
+path = '../work_journal/'
+files = os.listdir(path)
+
+num_files = len(files)
+total_hours = 0
+
+for i in files:
+    try:
+        fil = open(path + i, 'r')
+        total_hours += int(fil.readline())
+    except IOError:
+        print "Error reading from file"
+    except ValueError:
+        print "Missing value in: ", fil
+        
+print '  Total files: ', num_files
+print '  Total hours: ', total_hours
+print 'Average hours: ', total_hours/num_files
